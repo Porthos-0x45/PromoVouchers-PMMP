cd36647138a10a8e5e755e83d2689f11c34b7e59
Null-Pixel <porthossword@gmail.com>
2022-08-26 00:57:29 +0200

    Initial commit

---
+++ src/Px1L/PromoCodes/core/Main.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Px1L\PromoCodes\Core;
+
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\item\Item;
+use pocketmine\item\enchantment\Enchantment;
+use Px1L\PromoCodes\listners\InteractListener;
+
+class Main extends PluginBase {
+
+    public $config;
+
+    public function onLoad() : void
+    {
+		$this->getServer()->getLogger()->info(TextFormat::RED."I NEED SLEEEEP!!!!!!!!!!!!!");
+    }
+
+    public function onEnable() : void{
+		$this->getServer()->getLogger()->info(TextFormat::RED."I NEED SLEEEEP!!!!!!!!!!!!!");
+
+		$this->getServer()->getPluginManager()->registerEvents(new InteractListener($this), $this)
+		$config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+        $this->saveDefaultConfig();
+	}
+
+	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
+        $player = $this->getServer()->getPlayer($sender->getName());
+	    $server = $this->getServer();
+	    $nocmd = TextFormat::RED."You do not have permission to use this command";
+
+        if ($cmd->getName("promo"))
+        {
+            if ($sender instanceof Player)
+            {
+                if($sender->hasPermission("promo.create")){
+
+                    if (!isset($args[0]))
+                    {
+                        $player->sendMessage(TextFormat::GOLD . "/promo <name>");
+                    }
+    
+                    else {
+                        if (count($args) > 0 && count($args) == 1)
+                        {
+                            $name = $args[0];
+                            $inventory = $player->getInventory();
+                            $item = $player->getInventory()->getItemInHand();
+                            $enchantment = Enchantment::getEnchantment(17);
+                            $voucher = Item::get(339, 0, 1);
+                            
+                            $enchantment->setLevel(255);
+    
+                            $voucher->addEnchantment($enchantment);
+                            $voucher->setCustomName($name);
+    
+                            $config->setNested($name . ".ID",    $item->getId());
+                            $config->setNested($name . ".COUNT", $item->getCount());
+                            if ($item->hasEnchantments())
+                            {
+                                foreach ($item->getEnchantments() as $enchantment) {
+                                    $config->setNested($name . ".ENCHANT_ID", $enchantment->getId());
+                                    $config->setNested($name . ".ENCHANT_LVL", $enchantment->getLevel());
+                                }
+                            }
+    
+                            $config->save();
+    
+                            $inventory->setItemInHand(Item::get(Item::AIR));
+                            $inventory->addItem($voucher);
+
+                            $player->sendMessage(TextFormat::GREEN . "Nice! You made a promo voucher with name: " . $name);
+                        }
+                        else {
+                            $player->sendMessage(TextFormat::GOLD . "/promo <name>");
+
+                        }
+                    }
+                }
+                else {
+                    $player->sendMessage($nocmd);
+                }
+
+            } else {
+                $sender->sendMessage("Nope you need to be in game kido");
+            }
+        }
+    }
+}
+
+?>
---
+++ src/Px1L/PromoCodes/listeners/InteractListener.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Px1L\PromoCodes\listeners;
+
+use Pixel\AntiVoid\Core\Main;
+use pocketmine\level\Level;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\utils\TextFormat;
+use pocketmine\item\Item;
+use pocketmine\item\enchantment\Enchantment;
+
+class InteractListener implements Listener
+{
+    private $plugin;
+    private	$nocmd = TextFormat::RED."You do not have permission to use this command";
+
+
+    public function __construct(Main $plugin)
+    {
+        $this->plugin = $plugin;
+    }
+
+    public function onPlayerInteract(PlayerInteractEvent $event)
+    {
+        $player = $event->getPlayer();
+        $inventory = $player-getInventory();
+        
+        if($sender->hasPermission("promo.use")){
+            foreach ($plugin->config->getAll() as $name)
+            {
+                if ($inventory->getItemInHand()->getName() == $name)
+                {
+                    $item = Item::get((int)$plugin->config->getNested($name)["ID"], 0, (int)$plugin->config->getNested($name)["COUNT"])
+                    $enchantment = Enchantment::getEnchantment((int)$plugin->config->getNested($name)["ENCHANT_ID"]);
+                    $enchantment->setLevel((int) $plugin->config->getNested($name)["ENCHANT_LVL"]);
+
+                    $item->addEnchantment($enchantment);
+                    
+                    $inventory->setItemInHand(Item::get(Item::AIR));
+                    $inventory->setItemInHand($item);
+
+                    if ($inventory->getItemInHand()->getId() == (int)$plugin->config->getNested($name)["ID"])
+                    {
+                        $player->sendMessage(TextFormat::GREEN . "Congrats you lucky fucker get ready for 4 AM experience because you got ". TextFormat::GOLD .(int)$plugin->config->getNested($name)["COUNT"] .
+                                                    " ". TextFormat::BLUE . $item->getName());
+
+                        $plugin->config->remove($name);
+                    }
+                }
+            }
+        } else {
+            $player->sendMessage($nocmd);
+        }
+    }
+}
+
+?>
