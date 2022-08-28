05832698c578371bb9ab3ad9825b23a72d40cb17
Null-Pixel <porthossword@gmail.com>
2022-08-28 17:12:50 +0200

    Fixed

--- src/Px1L/PromoCodes/listeners/InteractListener.php
+++ src/Px1L/PromoCodes/listeners/InteractListener.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Px1L\PromoCodes\listeners;
 
 use Pixel\AntiVoid\Core\Main;
-use pocketmine\level\Level;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\utils\TextFormat;
@@ -14,9 +13,8 @@ use pocketmine\item\enchantment\Enchantment;
 
 class InteractListener implements Listener
 {
-    private $plugin;
-    private	$nocmd = TextFormat::RED."You do not have permission to use this command";
-
+    public $plugin;
+    public $nocmd = TextFormat::RED . "You do not have permission to use this command";
 
     public function __construct(Main $plugin)
     {
@@ -26,35 +24,31 @@ class InteractListener implements Listener
     public function onPlayerInteract(PlayerInteractEvent $event)
     {
         $player = $event->getPlayer();
-        $inventory = $player-getInventory();
-        
-        if($sender->hasPermission("promo.use")){
-            foreach ($plugin->config->getAll() as $name)
-            {
-                if ($inventory->getItemInHand()->getName() == $name)
-                {
+        $inventory = $player->getInventory();
+
+
+        if ($player->hasPermission("promo.use")) {
+            foreach ($plugin->config->getAll() as $name) {
+                if ($inventory->getItemInHand()->getName() == $name) {
                     $item = Item::get((int)$plugin->config->getNested($name)["ID"], 0, (int)$plugin->config->getNested($name)["COUNT"]);
                     $enchantment = Enchantment::getEnchantment((int)$plugin->config->getNested($name)["ENCHANT_ID"]);
                     $enchantment->setLevel((int) $plugin->config->getNested($name)["ENCHANT_LVL"]);
 
                     $item->addEnchantment($enchantment);
-                    
+
                     $inventory->setItemInHand(Item::get(Item::AIR));
                     $inventory->setItemInHand($item);
 
-                    if ($inventory->getItemInHand()->getId() == (int)$plugin->config->getNested($name)["ID"])
-                    {
-                        $player->sendMessage(TextFormat::GREEN . "Congrats you lucky fucker get ready for 4 AM experience because you got ". TextFormat::GOLD .(int)$plugin->config->getNested($name)["COUNT"] .
-                                                    " ". TextFormat::BLUE . $item->getName());
+                    if ($inventory->getItemInHand()->getId() == (int)$plugin->config->getNested($name)["ID"]) {
+                        $player->sendMessage(TextFormat::GREEN . "Congrats you lucky fucker get ready for 4 AM experience because you got " . TextFormat::GOLD . (int)$plugin->config->getNested($name)["COUNT"] .
+                            " " . TextFormat::BLUE . $item->getName());
 
                         $plugin->config->remove($name);
                     }
                 }
             }
         } else {
-            $player->sendMessage($nocmd);
+            $player->sendMessage($this->nocmd);
         }
     }
 }
-
-?>
