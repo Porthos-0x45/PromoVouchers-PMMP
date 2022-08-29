831d9b3cac52a3e3973b42b099484a33c7232008
Null-Pixel <porthossword@gmail.com>
2022-08-29 11:27:20 +0200

    more fixing uuh

--- src/Px1L/PromoCodes/Listener/InteractListener.php
+++ src/Px1L/PromoCodes/Listener/InteractListener.php
@@ -9,8 +9,6 @@ use Px1L\PromoCodes\Core\Main;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\utils\TextFormat;
-use pocketmine\item\Item;
-use pocketmine\item\enchantment\Enchantment;
 use pocketmine\item\ItemFactory;
 use pocketmine\item\ItemIds;
 
@@ -38,7 +36,11 @@ class InteractListener implements Listener
                 if ($inventory->getItemInHand()->getLore() == $lore) {
                     $name = $inventory->getItemInHand()->getName();
 
-                    $item = ItemFactory::getInstance()->get((int)$plugin->config->getNested($name)["ID"], 0, (int)$plugin->config->getNested($name)["COUNT"]);
+                    $item = ItemFactory::getInstance()->get(
+                        (int)$plugin->config->getNested($name)["ID"],
+                        (int)$plugin->config->getNested($name)["META"],
+                        (int)$plugin->config->getNested($name)["COUNT"]
+                    );
 
                     if ($plugin->config->getNested($name)["ENCHANT"] == true) {
                         $enchantment = EnchantmentIdMap::getInstance()->fromId((int)$plugin->config->getNested($name)["ENCHANT_ID"]);
@@ -47,15 +49,19 @@ class InteractListener implements Listener
                         $item->addEnchantment($enchantment);
                     }
 
-                    $inventory->setItemInHand(ItemFactory::getInstance()->get(ItemIds::AIR));
-                    $inventory->setItemInHand($item);
+                    //$inventory->setItemInHand(ItemFactory::getInstance()->get(ItemIds::AIR));
+                    //$inventory->setItemInHand($item);
+
+                    $inventory->removeItem($inventory->getItemInHand());
+                    $inventory->addItem($item);
 
                     if ($inventory->getItemInHand()->getId() == (int)$plugin->config->getNested($name)["ID"]) {
-                        $player->sendMessage(TextFormat::GREEN . "Congrats you lucky fucker get ready for 4 AM experience because you got " . TextFormat::GOLD . (int)$plugin->config->getNested($name)["COUNT"] .
+                        $player->sendMessage(TextFormat::GREEN . "Congrats you lucky f u c k e r get ready for 4 AM experience because you got " . TextFormat::GOLD . (int)$plugin->config->getNested($name)["COUNT"] .
                             " " . TextFormat::BLUE . $item->getName());
 
                         $plugin->config->remove($name);
                         $plugin->config->save();
+                        $event->cancel(true);
                     }
                 }
             }
--- src/Px1L/PromoCodes/core/Main.php
+++ src/Px1L/PromoCodes/core/Main.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Px1L\PromoCodes\Core;
 
-use pocketmine\event\Listener;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\data\bedrock\EnchantmentIdMap;
@@ -12,13 +11,10 @@ use pocketmine\data\bedrock\EnchantmentIds;
 use pocketmine\utils\TextFormat;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
-use pocketmine\item\Item;
-use pocketmine\item\enchantment\Enchantment;
 use pocketmine\item\enchantment\EnchantmentInstance;
-use pocketmine\item\enchantment\ProtectionEnchantment;
-use pocketmine\item\enchantment\VanillaEnchantments;
 use pocketmine\item\ItemFactory;
 use pocketmine\item\ItemIds;
+use pocketmine\item\VanillaItems;
 use pocketmine\player\Player;
 use Px1L\PromoCodes\Listener\InteractListener;
 
@@ -53,44 +49,84 @@ class Main extends PluginBase
                 if ($sender->hasPermission("promo.create")) {
 
                     if (!isset($args[0])) {
-                        $player->sendMessage(TextFormat::GOLD . "/promo <name>");
+                        $player->sendMessage(TextFormat::GOLD . "/promo create <name> | /promo remove <name> | /promo get <name>");
                     } else {
-                        if (count($args) > 0 && count($args) == 1) {
-                            $name = $args[0];
-                            $inventory = $player->getInventory();
-                            $item = $player->getInventory()->getItemInHand();
-                            $voucher = ItemFactory::getInstance()->get(ItemIds::PAPER);
-                            $enchantments = new EnchantmentInstance(EnchantmentIdMap::getInstance()->fromId(EnchantmentIds::UNBREAKING), 255);
+                        if (count($args) > 0 && count($args) == 2) {
+                            if ($args[0] == "create") {
+                                $name = $args[1];
 
-                            $voucher->addEnchantment($enchantments);
-                            $voucher->setCustomName($name);
-                            $voucher->setLore(array(
-                                TextFormat::AQUA . "Voucher created by " . $player->getName(),
-                                TextFormat::RESET . " ",
-                                TextFormat::RED . TextFormat::ITALIC . "Plugin by Px1L"
-                            ));
+                                if ($name != $config->exists($name)) {
+                                    $inventory = $player->getInventory();
+                                    $item = $player->getInventory()->getItemInHand();
+                                    $voucher = ItemFactory::getInstance()->get(ItemIds::PAPER);
+                                    $enchantments = new EnchantmentInstance(EnchantmentIdMap::getInstance()->fromId(EnchantmentIds::UNBREAKING), 255);
 
-                            $config->setNested($name . ".ID",    $item->getId());
-                            $config->setNested($name . ".COUNT", $item->getCount());
+                                    $voucher->addEnchantment($enchantments);
+                                    $voucher->setCustomName($name);
+                                    $voucher->setLore(array(
+                                        TextFormat::AQUA . "Voucher created by " . $player->getName(),
+                                        TextFormat::RESET . " ",
+                                        TextFormat::RED . TextFormat::ITALIC . "Plugin by Px1L"
+                                    ));
 
-                            if ($item->hasEnchantments()) {
-                                $config->setNested($name . ".ENCHANT", true);
-                                foreach ($item->getEnchantments() as $enchantment) {
-                                    $config->setNested($name . ".ENCHANT_ID", EnchantmentIdMap::getInstance()->toId($enchantment));
-                                    $config->setNested($name . ".ENCHANT_LVL", $enchantment->getLevel());
+                                    $config->setNested($name . ".ID",    $item->getId());
+                                    $config->setNested($name . ".COUNT", $item->getCount());
+                                    $config->setNested($name . ".META", $item->getMeta());
+
+                                    if ($item->hasEnchantments()) {
+                                        $config->setNested($name . ".ENCHANT", true);
+                                        foreach ($item->getEnchantments() as $enchantment) {
+                                            $config->setNested($name . ".ENCHANT_ID", EnchantmentIdMap::getInstance()->toId($enchantment));
+                                            $config->setNested($name . ".ENCHANT_LVL", $enchantment->getLevel());
+                                        }
+                                    } else {
+                                        $config->setNested($name . ".ENCHANT", false);
+                                    }
+
+                                    $config->save();
+
+                                    $inventory->removeItem($inventory->getItemInHand());
+                                    $inventory->addItem($voucher);
+
+                                    $player->sendMessage(TextFormat::GREEN . "Nice! You made a promo voucher with name: " . $name);
+                                } else {
+                                    $player->sendMessage(TextFormat::GOLD . "'" . $name . "'" . TextFormat::RED . " already exists. Try different name.");
                                 }
-                            } else {
-                                $config->setNested($name . ".ENCHANT", false);
+                            } else if ($args[0] == "remove") {
+                                $name = $args[1];
+                                if ($name == $config->exists($name)) {
+                                    $config->remove($name);
+                                } else {
+                                    $player->sendMessage(TextFormat::AQUA . $name . TextFormat::RED . "Doesn't exists!");
+                                }
+                            } else if ($args[0] == "get") {
+                                $name = $args[1];
+
+                                if ($name == $config->exists($name)) {
+
+                                    $inventory = $player->getInventory();
+                                    $item = $player->getInventory()->getItemInHand();
+                                    $voucher = ItemFactory::getInstance()->get(ItemIds::PAPER);
+                                    $enchantments = new EnchantmentInstance(EnchantmentIdMap::getInstance()->fromId(EnchantmentIds::UNBREAKING), 255);
+
+                                    $voucher->addEnchantment($enchantments);
+                                    $voucher->setCustomName($name);
+                                    $voucher->setLore(array(
+                                        TextFormat::AQUA . "Voucher created by " . $player->getName(),
+                                        TextFormat::RESET . " ",
+                                        TextFormat::RED . TextFormat::ITALIC . "Plugin by Px1L"
+                                    ));
+
+                                    if ($item !== VanillaItems::AIR())
+                                        $inventory->removeItem($inventory->getItemInHand());
+
+                                    $inventory->addItem($voucher);
+                                } else {
+                                    $player->sendMessage(TextFormat::AQUA . $name . TextFormat::RED . "Doesn't exists!");
+                                }
                             }
-
-                            $config->save();
-
-                            $inventory->setItemInHand(ItemFactory::getInstance()->get(ItemIds::AIR));
-                            $inventory->addItem($voucher);
-
-                            $player->sendMessage(TextFormat::GREEN . "Nice! You made a promo voucher with name: " . $name);
                         } else {
-                            $player->sendMessage(TextFormat::GOLD . "/promo <name>");
+                            $player->sendMessage(TextFormat::GOLD . "/promo create <name> | /promo remove <name> | /promo get <name>");
                         }
                     }
                 } else {
